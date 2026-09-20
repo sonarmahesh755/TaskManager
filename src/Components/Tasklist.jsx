3769472d86a279ad2455371cdff9c2d607c432d2
sonarmahesh755 <sonarmahesh755@gmail.com>
2026-09-20 14:18:00 +0530

    final project

---
+++ src/Components/Tasklist.jsx
@@ -0,0 +1,30 @@
+export default function TaskList({tasks, updateTask, deleteTask}) {
+  const toggleComplete = (index)=>{
+    const updatedTask = {...tasks[index], completed: !tasks[index].completed};
+    updateTask(updatedTask, index);
+  }
+  return (
+    <ul className='task-list'>
+      {tasks.map((task, index)=>(
+        <li key={index}>
+          <div>
+            <span>{task.text}</span>
+            <small>({task.priority}, {task.category})</small>
+          </div>
+
+          <div>
+            <button onClick={()=>toggleComplete(index)}
+              >{task.completed ? "Undo" : "Complete"}</button>
+            <button
+  className="delete-btn"
+  onClick={() => deleteTask(index)}
+>
+  Delete
+</button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
